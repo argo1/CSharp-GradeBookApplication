089150fc258ee96f5cc21cdb988c7153863c3498
Argo Vessmann <arve@microsoft.com>
2019-07-17 20:01:53 +0300

    Update RankedGradeBook.cs
    fix else

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,16 +34,17 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            else
 
-
-            base.CalculateStatistics();
+                base.CalculateStatistics();
         }
 
         public override void CalculateStudentStatistics(string name)
         {
             if (Students.Count < 5)
                 Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-            base.CalculateStudentStatistics(name);
+            else
+                base.CalculateStudentStatistics(name);
         }
     }
 
