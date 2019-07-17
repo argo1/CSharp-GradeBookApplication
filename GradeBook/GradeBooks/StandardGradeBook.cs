bcbc11241c70bacc1b1d31d9f1db634aba6ba216
Argo Vessmann <arve@microsoft.com>
2019-07-17 19:39:18 +0300

    Rest

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,7 +4,8 @@ namespace GradeBook.GradeBooks
     public class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(string name) : base(name)
-        {
+        { 
+            Type = Enums.GradeBookType.Standard;
         }
     }
 }
